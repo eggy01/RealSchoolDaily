b330f18b21ab46d97320d65a0b40d829b6e16205
WeekWind323 <2923621891@qq.com>
2025-04-18 23:27:16 +0800

    Revert "Merge branch 'Bag'"
    This reverts commit 98c40ed8fa88c35b64135979833a9331c4fd0e61, reversing
    changes made to 8646191dd9b5e4e7a45882d69cf8bf3dfa57d56a.

--- SchoolDaily/Assets/Scripts/UI/Drag.cs
+++ SchoolDaily/Assets/Scripts/UI/Drag.cs
@@ -1,32 +1,43 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(ScrollRect))]
-public class ForceScrollWheel : MonoBehaviour
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
 {
-    public float scrollSensitivity = 1f; // 滚轮灵敏度
-    private ScrollRect scrollRect;
+    private ScrollRect parentScrollRect;
 
-    void Start()
+    private void Start()
     {
-        scrollRect = GetComponent<ScrollRect>();
+        parentScrollRect = GetComponentInParent<ScrollRect>();
     }
 
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        // 检测鼠标滚轮输入
-        float wheelInput = Input.GetAxis("Mouse ScrollWheel");
-        
-        if (wheelInput != 0 && scrollRect.vertical) // 如果是垂直滚动
+        if (parentScrollRect != null)
         {
-            // 直接调整滚动位置
-            scrollRect.verticalNormalizedPosition += wheelInput * scrollSensitivity;
-            scrollRect.verticalNormalizedPosition = Mathf.Clamp(scrollRect.verticalNormalizedPosition, 0f, 1f);
+            parentScrollRect.OnBeginDrag(eventData);
         }
-        else if (wheelInput != 0 && scrollRect.horizontal) // 如果是水平滚动
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (parentScrollRect != null)
         {
-            scrollRect.horizontalNormalizedPosition += wheelInput * scrollSensitivity;
-            scrollRect.horizontalNormalizedPosition = Mathf.Clamp(scrollRect.horizontalNormalizedPosition, 0f, 1f);
+            parentScrollRect.OnDrag(eventData);
         }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (parentScrollRect != null)
+        {
+            parentScrollRect.OnEndDrag(eventData);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // 触发点击事件
+        Debug.Log("Clicked: " + gameObject.name);
     }
 }
